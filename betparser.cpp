f626b44480104f2532a28a8c17a1aac9278ddf06
IgorLisovoy <igor.lisovoy@yandex.ru>
2014-01-05 17:49:56 +0200

    add class-method connectSignalSlot

--- betparser.cpp
+++ betparser.cpp
@@ -1,14 +1,32 @@
 #include "betparser.h"
 #include "ui_betparser.h"
 
+/*
+    constructor
+*/
 betParser::betParser(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::betParser)
 {
     ui->setupUi(this);
+    ui->groupBox_4->hide();
+
+    // connect SIGNAL and SLOTS
+    connectSignalSlots();
 }
 
+/*
+    destructor. Remove some variables
+*/
 betParser::~betParser()
 {
     delete ui;
+}
+
+/*
+  connect signal-function to slot(methods)-function
+*/
+void betParser::connectSignalSlots()
+{
+    connect(ui->toolButton, SIGNAL(clicked()), ui->groupBox_4, SLOT(show()));
 }
